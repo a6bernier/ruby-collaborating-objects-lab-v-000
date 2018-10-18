c3576f00b1562c9c97c63bd352a9893bd14aee60
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 19:10:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self)
+    artist.add_song(self) unless artist.@songs.include?(self)
   end
 
 #binding.pry
