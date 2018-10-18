2e9b075e6209c565529d7d6bf609a0e9164b6540
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 18:10:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,10 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist_name, song_name, genre_name = filename.chomp.split(" - ")[0]
+    artist_name, song_name, genre_name = filename.chomp.split(" - ")
+    song = self.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = genre_name
   end
 
   def artist=(artist) #allows both objects to know about relationship
