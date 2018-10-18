d47edaa3b19a6a33a2e7cc72e23dbe90f5ce28ac
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 19:06:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   end
 
   def artist=(artist)
-    @artist = artist
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self) unless artist.songs.include?(self)
   end
 
