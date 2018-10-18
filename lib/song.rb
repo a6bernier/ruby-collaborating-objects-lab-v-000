faa92e565be291e0b843b41786849dd06bd61218
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 21:03:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,22 +21,22 @@ class Song
     self
   end
 
-  # def self.new_by_filename(filename)
-  #   artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
-  #   song = self.new(song_name)
-  #   song.artist = artist_name
-  #   song.genre = genre_name
-  #   song.save
-  # end
-
-  def self.new_by_filename(file)
-    song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
+  def self.new_by_filename(filename)
+    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
     song = self.new(song_name)
-
-    song.artist_name = artist
-    song
+    song.artist = artist_name
+    song.genre = genre_name
+    song.save
   end
+
+  # def self.new_by_filename(file)
+  #   song_name = file.split(" - ")[1]
+  #   artist = file.split(" - ")[0]
+  #   song = self.new(song_name)
+  #
+  #   song.artist_name = artist
+  #   song
+  # end
 
 
 
