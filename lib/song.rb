b0c97c2cd77040c7fad7d43d23d0ec61aa7c41af
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 19:18:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,50 +1,59 @@
-require "pry"
+# require "pry"
+#
+# class Song
+#
+#   attr_accessor :genre, :name
+#   attr_reader :artist
+#
+#   def initialize(name)
+#     @name = name
+#     @artist = nil
+#   end
+#
+#   @@all = []
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def save
+#     @@all << self
+#     self
+#   end
+#
+#   def self.new_by_filename(filename)
+#     artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
+#     song = self.new(song_name)
+#     song.artist = Artist.find_or_create_by_name(artist_name)
+#     song.genre = genre_name
+#     song.save
+#   end
+#
+#   def artist=(artist)
+#     @artist = artist
+#     artist.add_song(self) unless artist.songs.include?(self)
+#   end
+#
+# end #ends class Song
 
 class Song
 
-  attr_accessor :genre, :name
-  attr_reader :artist
+  attr_accessor :artist, :name
 
   def initialize(name)
     @name = name
-    @artist = nil
   end
 
-  @@all = []
-
-  def self.all
-    @@all
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
-  def save
-    @@all << self
-    self
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
 
-  def self.new_by_filename(filename)
-    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
-    song = self.new(song_name)
-    song.artist = Artist.find_or_create_by_name(artist_name)
-    song.genre = genre_name
-    song.save
-  end
-
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self) unless artist.songs.include?(self)
-  end
-
-#binding.pry
-# it 'associates new song instance with the artist from the filename' do
-#   Artist.class_variable_set("@@all",[])
-#   new_instance = Song.new_by_filename(file_name)
-#   expect(new_instance.artist.name).to eq('Michael Jackson')
-#   expect(Artist.all.size).to eq(1)
-#   expect(Artist.all.first.songs.empty?).to eq(false)
-# end
-# end
-# end
-
-
-
-end #ends class Song
+end
