f23049043d5b44c7f29b528b70c7ac2934b389fc
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 19:04:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,21 @@ class Song
     song.save
   end
 
-  def artist=(artist) 
+  def artist=(artist)
     @artist = artist
     artist.add_song(self) unless artist.songs.include?(self)
   end
 
 binding.pry
-
+# it 'associates new song instance with the artist from the filename' do
+#   Artist.class_variable_set("@@all",[])
+#   new_instance = Song.new_by_filename(file_name)
+#   expect(new_instance.artist.name).to eq('Michael Jackson')
+#   expect(Artist.all.size).to eq(1)
+#   expect(Artist.all.first.songs.empty?).to eq(false)
+# end
+# end
+# end
 
 
 
