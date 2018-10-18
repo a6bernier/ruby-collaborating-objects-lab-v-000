bc75885929692ecba71fc72b9c8f9ebbe4403898
Andrew Bernier <a6bernier@gmail.com>
2018-10-18 21:13:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,9 +39,10 @@ class Song
   # end
 
   def artist_name=(name)
-    binding.pry
+  
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
+      binding.pry
   end
 
 
